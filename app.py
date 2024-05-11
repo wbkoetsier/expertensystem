7b17c6f40445e420bd02db67850d0f828fdf2078
Wil Koetsier <wbkoetsier@users.noreply.github.com>
2024-05-11 13:15:44 +0000

    feat: Switch van flask naar pyramid

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from waitress import serve
+from pyramid.config import Configurator
+from pyramid.response import Response
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', title='Hello, World!')
+def hello_world(request):
+    print('Incoming request')
+    return Response('<body><h1>Hello World!</h1></body>')
+
 
 if __name__ == '__main__':
-    app.run()
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    serve(app, host='0.0.0.0', port=6543)
